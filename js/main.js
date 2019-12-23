b16b234f00b5a08c0d270005a48f86d4fe2e4928
xuanngo10 <xuan.ngo10@gmail.com>
2019-12-23 11:21:19 -0800

    fixed errors and added mobile media queries

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $("input").keyup(function() {
   const $searchInput = $(this)
     .val()
     .toLowerCase();
-  $("a").each(function() {
+  $(".link-container a").each(function() {
     const $capt = $(this).attr("data-title");
     if ($capt.indexOf($searchInput) > -1) {
       $(this).show();
